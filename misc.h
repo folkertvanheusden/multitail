75472dd5c88b43855529204e330cd3469625ca8e
Pascal Jäger <pascal.jaeger@leimstift.de>
2022-11-01 22:41:12 +0100

    fix build for clang16

--- misc.h
+++ misc.h
@@ -1,5 +1,7 @@
+#import "mt.h"
+
 void info(void);
 void statistics_menu(void);
 void heartbeat(void);
-void do_check_for_mail();
+void do_check_for_mail(dtime_t time);
 void store_statistics(proginfo *cur, dtime_t now);
